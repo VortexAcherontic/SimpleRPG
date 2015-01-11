5a7d63b7b66af2578e7f7935e07a783ef2025679
Sascha Kaufmann <Sascha.u.kaufmann@googlemail.com>
2015-01-11 22:49:03 +0100

    Hinweis Map Laden

--- Assets/Map/map.cs
+++ Assets/Map/map.cs
@@ -1,31 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class map : MonoBehaviour
-{
+public class map : MonoBehaviour {
 
 		public Texture minimap;
 		public mainmenu gui;
 		public player p001;
 
 		// Use this for initialization
-		void Start ()
-		{
+		void Start () {
 				gui = GameObject.Find ("Main Camera").GetComponent<mainmenu> ();
 				p001 = GameObject.Find ("Main Camera").GetComponent<player> ();
 		}
 
-		void OnGUI ()
-		{
+		void OnGUI () {
 				if (gui.showmap) {
+						// Map laden... http://docs.unity3d.com/ScriptReference/Resources.Load.html
 						GUI.DrawTexture (new Rect (1180, 0, 100, 100), minimap);
 				}
 				//if(p001.pos)
 		}
 
 		// Update is called once per frame
-		void Update ()
-		{
+		void Update () {
 	
 		}
 }
