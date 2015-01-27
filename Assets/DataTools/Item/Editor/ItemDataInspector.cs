a1c0d3579832ba770345b7279663e3f4915fd507
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2015-01-27 17:37:50 +0100

    Begin Create Item Struct DataTools

---
+++ Assets/DataTools/Item/Editor/ItemDataInspector.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+/*
+[CustomPropertyDrawer(typeof(ItemOriginStats))]
+public class ItemOriginStatsDrawer: PropertyDrawer {
+		public override void OnGUI (Rect pos, SerializedProperty prop, GUIContent label) {
+				DrawDefaultInspector();
+		}
+}
+*/
+
+/*
+[CustomPropertyDrawer(typeof(ItemStats))]
+public class ItemStatsDrawer: PropertyDrawer {
+		public override void OnGUI (Rect pos, SerializedProperty prop, GUIContent label) {
+
+	}
+}
+*/
+
+public class MakeItemData {
+		[MenuItem("DataTools/Create/ItemList")]
+		public static void CreateItemData () {
+				ItemDataList asset = ScriptableObject.CreateInstance<ItemDataList> ();
+				AssetDatabase.CreateAsset (asset, "Assets/DataTools/Item/Resources/Items.asset");
+				AssetDatabase.SaveAssets ();
+		
+				EditorUtility.FocusProjectWindow ();
+				Selection.activeObject = asset;
+		}
+}
