ba2c4387edde33cd16cf00abbea69767a49d38f9
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2015-02-09 23:06:13 +0100

    Minimap change on Teleport

--- Assets/Map/TileMap.cs
+++ Assets/Map/TileMap.cs
@@ -17,9 +17,9 @@ public class TileMap : MonoBehaviour {
 		public List<regions> tileTypes = new List<regions> ();
 		public int[,] tiles;
 	
-	int map_abstand=30; // Sollte So Groß sein wie der Spawn Radius!
-	int mapSizeX;
-	int mapSizeY;
+		int map_abstand = 30; // Sollte So Groß sein wie der Spawn Radius!
+		int mapSizeX;
+		int mapSizeY;
 
 		regions CreateEmpty () {
 				regions tmpreg = new regions ();
@@ -35,38 +35,39 @@ public class TileMap : MonoBehaviour {
 				return	tmpreg;
 		}
 
-	void OnGUI() {
-		// Ich will die farben sehen
-		bool farbensehen = false;
-		if (farbensehen) {
-			int breite=300;
-			GUILayout.BeginArea (new Rect(Screen.width-breite,0,breite,Screen.height)); {
-				foreach (regions tmpregion in tileTypes) {
-					GUILayout.BeginHorizontal();
-					GUILayout.Label (""+tmpregion.name);
-					GUILayout.Space (5);
-					Color32 tmpcolor=tmpregion.mapColor;
-					GUILayout.Label (""+tmpcolor.r);
-					GUILayout.Label (""+tmpcolor.g);
-					GUILayout.Label (""+tmpcolor.b);
-					GUILayout.Label (""+tmpcolor.a);
-					GUILayout.EndHorizontal();
+		void OnGUI () {
+				// Ich will die farben sehen
+				bool farbensehen = false;
+				if (farbensehen) {
+						int breite = 300;
+						GUILayout.BeginArea (new Rect (Screen.width - breite, 0, breite, Screen.height));
+						{
+								foreach (regions tmpregion in tileTypes) {
+										GUILayout.BeginHorizontal ();
+										GUILayout.Label ("" + tmpregion.name);
+										GUILayout.Space (5);
+										Color32 tmpcolor = tmpregion.mapColor;
+										GUILayout.Label ("" + tmpcolor.r);
+										GUILayout.Label ("" + tmpcolor.g);
+										GUILayout.Label ("" + tmpcolor.b);
+										GUILayout.Label ("" + tmpcolor.a);
+										GUILayout.EndHorizontal ();
+								}
+						}
+						GUILayout.EndArea ();
 				}
-			}
-			GUILayout.EndArea ();
 		}
-	}
 	
 		void Start () {
-			regions tmpreg = CreateEmpty (); // Void Tile für Abstand zwischen den Karten
-			tmpreg.isWalkable = false;
-			tileTypes.Add (tmpreg);
+				regions tmpreg = CreateEmpty (); // Void Tile für Abstand zwischen den Karten
+				tmpreg.isWalkable = false;
+				tileTypes.Add (tmpreg);
 		
-			tmpreg = CreateEmpty ();
-			tmpreg.name = "Portal";
-			tmpreg.tileVisualPrefab = (GameObject)Resources.Load ("Tiles/TileDoor");
-			tmpreg.walkEffect = 1;
-			tileTypes.Add (tmpreg);
+				tmpreg = CreateEmpty ();
+				tmpreg.name = "Portal";
+				tmpreg.tileVisualPrefab = (GameObject)Resources.Load ("Tiles/TileDoor");
+				tmpreg.walkEffect = 1;
+				tileTypes.Add (tmpreg);
 		
 				tmpreg = CreateEmpty ();
 				tmpreg.name = "Forest";
@@ -197,10 +198,10 @@ public class TileMap : MonoBehaviour {
 		
 				List<Texture2D> ListMap = gameObject.GetComponent<map> ().Maps;
 				foreach (Texture2D AktlMap in ListMap) {
-					mapSizeX += map_abstand + AktlMap.height;
-					if (2*map_abstand + AktlMap.width>=mapSizeY) {
-							mapSizeY += map_abstand + AktlMap.width;
-					}
+						mapSizeX += map_abstand + AktlMap.height;
+						if (2 * map_abstand + AktlMap.width >= mapSizeY) {
+								mapSizeY += map_abstand + AktlMap.width;
+						}
 				}
 				tiles = new int[mapSizeX, mapSizeY];
 				int x, y;
@@ -212,20 +213,20 @@ public class TileMap : MonoBehaviour {
 		
 				// Nun alle Bilder durchgehen
 				foreach (Texture2D AktlMap in ListMap) {
-			for (x = 0; x < AktlMap.height; x++) {
-				for (y = 0; y < AktlMap.width; y++) {
-									Color FeldFarbe = AktlMap.GetPixel (x, y);
+						for (x = 0; x < AktlMap.height; x++) {
+								for (y = 0; y < AktlMap.width; y++) {
+										Color FeldFarbe = AktlMap.GetPixel (x, y);
 										// Viele Stellen genau sollte das sein? Ich denke 3 muss reichen
 										FeldFarbe.r = Mathf.Round (FeldFarbe.r * 1000) / 1000;
 										FeldFarbe.g = Mathf.Round (FeldFarbe.g * 1000) / 1000;
 										FeldFarbe.b = Mathf.Round (FeldFarbe.b * 1000) / 1000;
 										FeldFarbe.a = Mathf.Round (FeldFarbe.a * 1000) / 1000;
 										// Tiles Ziffern noch anpassen
-										tiles [start_x+x, start_y+y] = GetRegionWithColor (FeldFarbe);
+										tiles [start_x + x, start_y + y] = GetRegionWithColor (FeldFarbe);
 								}
 						}
-					start_x += map_abstand + AktlMap.height;
-					//start_y += map_abstand + AktlMap.width;
+						start_x += map_abstand + AktlMap.height;
+						//start_y += map_abstand + AktlMap.width;
 				}
 				CheckForPortals ();
 				GenerateMapVisuals ();
@@ -242,9 +243,10 @@ public class TileMap : MonoBehaviour {
 		}
 	
 		void CheckForPortals () {
+				GameObject.Find ("Main Camera").GetComponent<teleporter> ().LoadPorter ();
 				List<ObjTele> tmplist = GameObject.Find ("Main Camera").GetComponent<teleporter> ().Porter;
 				foreach (ObjTele tmpobj in tmplist) {
-					tiles [(int)tmpobj.vonpos.x,(int)tmpobj.vonpos.y] = GetRegionWithName ("Portal");
+						tiles [(int)tmpobj.vonpos.x, (int)tmpobj.vonpos.y] = GetRegionWithName ("Portal");
 				}
 		}
 
--- Assets/Objecte/teleporter.cs
+++ Assets/Objecte/teleporter.cs
@@ -3,41 +3,51 @@ using System.Collections;
 using System.Collections.Generic;
 
 public struct ObjTele {
-	//public string vonmap;
-	public Vector2 vonpos;
-	//public string nachmap;
-	public Vector2 nachpos;
+		//public string vonmap;
+		public Vector2 vonpos;
+		//public string nachmap;
+		public Vector2 nachpos;
+		public Texture2D minimap_to_update;
 }
 public class teleporter : MonoBehaviour {
-	public List<ObjTele> Porter = new List<ObjTele>();
-	player p001;
-	
-	void Start () {
-		p001 = GameObject.Find ("Main Camera").GetComponent<player> ();
-		AddTeleporter (new Vector2 (75, 81), new Vector2 (161, 56));
-		AddTeleporter (new Vector2 (75, 80), new Vector2 (161, 55));
-		AddTeleporter (new Vector2 (75, 79), new Vector2 (161, 54));
+		public List<ObjTele> Porter = new List<ObjTele> ();
+		player p001;
+		map m001;
 		
-		AddTeleporter (new Vector2 (160, 56), new Vector2 (74, 81));
-		AddTeleporter (new Vector2 (160, 55), new Vector2 (74, 80));
-		AddTeleporter (new Vector2 (160, 54), new Vector2 (74, 79));
-	}
 	
-	public void AddTeleporter(Vector2 vonPos,Vector2 nachPos) {
-		ObjTele tmpporter;
-		tmpporter.vonpos = vonPos;;
-		tmpporter.nachpos = nachPos;
-		Porter.Add (tmpporter);
-	}
-	// Update is called once per frame
-	void Update () {
-			foreach (ObjTele tmpporter in Porter) {
-				if (tmpporter.vonpos==p001.pos) {
-					p001.pos=tmpporter.nachpos;
-					//GameObject.Find ("Map").GetComponent<map> ().LoadMap (p001.map);
-					p001.pos=tmpporter.nachpos;	
-					GameObject.Find ("Unit").GetComponent<PlayerToPos> ().MovePlayer ();
+		void Start () {
+				p001 = GameObject.Find ("Main Camera").GetComponent<player> ();
+				m001 = GameObject.Find ("Map").GetComponent<map> ();
+				
+		}
+	
+		public void AddTeleporter (Vector2 vonPos, Vector2 nachPos, Texture2D newMinimap) {
+				ObjTele tmpporter;
+				tmpporter.vonpos = vonPos;		
+				tmpporter.nachpos = nachPos;
+				tmpporter.minimap_to_update = newMinimap;
+				Porter.Add (tmpporter);
+				
+		}
+		// Update is called once per frame
+		void Update () {
+				foreach (ObjTele tmpporter in Porter) {
+						if (tmpporter.vonpos == p001.pos) {
+								p001.pos = tmpporter.nachpos;
+								p001.pos = tmpporter.nachpos;	
+								m001.minimap = tmpporter.minimap_to_update;
+								GameObject.Find ("Unit").GetComponent<PlayerToPos> ().MovePlayer ();
+						}
 				}
-			}
+		}
+		
+		public void LoadPorter () {
+				AddTeleporter (new Vector2 (75, 81), new Vector2 (161, 56), m001.Maps [1]);
+				AddTeleporter (new Vector2 (75, 80), new Vector2 (161, 55), m001.Maps [1]);
+				AddTeleporter (new Vector2 (75, 79), new Vector2 (161, 54), m001.Maps [1]);
+		
+				AddTeleporter (new Vector2 (160, 56), new Vector2 (74, 81), m001.Maps [0]);
+				AddTeleporter (new Vector2 (160, 55), new Vector2 (74, 80), m001.Maps [0]);
+				AddTeleporter (new Vector2 (160, 54), new Vector2 (74, 79), m001.Maps [0]);
 		}
 }
