b29a838fa0e523c60204b4bc27d8f020c2335fe3
Defender833 <martink999@web.de>
2015-02-15 00:13:39 +0100

    blub nur mal offen gewesen

--- Assets/Player_Files/Player_Save.cs
+++ Assets/Player_Files/Player_Save.cs
@@ -6,26 +6,23 @@ using System.Collections.Generic;
 
 
 
-public class Player_Save : MonoBehaviour
-{
-	// Erstmal nur Lokal Game!
-	// Klappt erstmal nur in UNITY!
+public class Player_Save : MonoBehaviour {
+		// Erstmal nur Lokal Game!
+		// Klappt erstmal nur in UNITY!
 	
-	public void Save ()
-	{
-		Savegame Game = ScriptableObject.CreateInstance<Savegame> ();
-		Game.Creature = gameObject.GetComponent<PlayerBehaviour> ().me.Creat;
+		public void Save () {
+				Savegame Game = ScriptableObject.CreateInstance<Savegame> ();
+				Game.Creature = gameObject.GetComponent<PlayerBehaviour> ().me.Creat;
 		
-		AssetDatabase.CreateAsset (Game, "Assets/Resources/" + Game.Creature.Name + ".asset");
-	}
+				AssetDatabase.CreateAsset (Game, "Assets/Resources/" + Game.Creature.Name + ".asset");
+		}
 	
-	public void Load (string Playername)
-	{
-		//Savegame Game = ScriptableObject.CreateInstance<Savegame> ();
-		Savegame Game = (Savegame)Resources.Load (Playername);
-		gameObject.GetComponent<PlayerBehaviour> ().me.Creat = Game.Creature;
-		transform.FindChild ("UnitModel").GetComponent<MeshRenderer> ().enabled = true;
-		GameObject.Find ("Map").GetComponent<map> ().LoadMap ();
-		gameObject.GetComponent<PlayerBehaviour> ().me.IsLoaded = true;
-	}
+		public void Load (string Playername) {
+				//Savegame Game = ScriptableObject.CreateInstance<Savegame> ();
+				Savegame Game = (Savegame)Resources.Load (Playername);
+				gameObject.GetComponent<PlayerBehaviour> ().me.Creat = Game.Creature;
+				transform.FindChild ("UnitModel").GetComponent<MeshRenderer> ().enabled = true;
+				GameObject.Find ("Map").GetComponent<map> ().LoadMap ();
+				gameObject.GetComponent<PlayerBehaviour> ().me.IsLoaded = true;
+		}
 }
