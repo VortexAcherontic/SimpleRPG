4c5bf9a75c1ef9fe62e3825485d70ec648fbf56d
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2015-02-19 22:43:07 +0100

    Doppelter Login wurde erstellt... jetzt nicht mehr

--- php/login.php
+++ php/login.php
@@ -1,18 +1,18 @@
 <?php
 
     // Alle Vars die Übergeben werden
-    $user_name = $_POST['user_name'];
+    $user_name = strtolower($_POST['user_name']);
     $user_password = sha1(md5($_POST['user_password']));
     // Alle Incs
     include_once("db.php"); // damit das auf der Page klappt
 
     //Login Check
-    $user_data = $datenbank->sql_select("user", "*", "user_name='" . $user_name . "'");
+    $user_data = $datenbank->sql_select("user", "*", "user_name like '" . $user_name . "'");
     $new_werte['user_name'] = $user_name;
-    if (!isset($user_data[0]['user_id'])) {
-	$new_werte['user_password'] = $user_password;
-	$datenbank->sql_insert_update("user", $new_werte);
-	$user_data = $datenbank->sql_select("user", "*", "user_name='" . $user_name . "'");
+    if (!isset($user_data[0]['id'])) {
+		$new_werte['user_password'] = $user_password;
+		$datenbank->sql_insert_update("user", $new_werte);
+		$user_data = $datenbank->sql_select("user", "*", "user_name='" . $user_name . "'");
     }
     unset($new_werte);
 	
