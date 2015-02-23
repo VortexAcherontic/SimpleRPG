0462dbf03882ba6db7676c2559ef3ac5820a75cf
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2015-02-23 18:24:28 +0100

    TextureScale jetzt auch am Boden

---
+++ Assets/Objecte/TextureSacleToTransformScale.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureSacleToTransformScale : MonoBehaviour {
+		public MeshRenderer ObjRenderer;
+		// Use this for initialization
+		void Start () {
+				ObjRenderer = GetComponent<MeshRenderer> ();
+				ObjRenderer.material.SetTextureScale ("_MainTex", new Vector2 (transform.localScale.x / 10, transform.localScale.z / 10));
+		}
+}
