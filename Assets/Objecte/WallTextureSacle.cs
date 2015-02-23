c232029b52a12f3bfe9a2b65a7be91fb47741628
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2015-02-23 18:12:26 +0100

    Wall Texture Scale

---
+++ Assets/Objecte/WallTextureSacle.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallTextureSacle : MonoBehaviour {
+		public MeshRenderer WallRenderer;
+		// Use this for initialization
+		void Start () {
+				WallRenderer = GetComponent<MeshRenderer> ();
+				WallRenderer.material.SetTextureScale ("_MainTex", new Vector2 (transform.localScale.x / 10, transform.localScale.z / 10));
+		}
+}
