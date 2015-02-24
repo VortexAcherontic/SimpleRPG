d22689941920df42ffbf821abcff6b6acd11c23f
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2015-02-24 21:16:49 +0100

    Camera Drehen. Idee Gut, Ausführung noch nicht

---
+++ Assets/Objecte/EditorPosToObj.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class EditorPosToObj : MonoBehaviour {
+		public Transform MoveTo;
+	
+		// Use this for initialization
+		void Update () {
+				if (MoveTo != null) {
+						transform.position = MoveTo.position;
+				}
+		}
+	
+}
--- Assets/Player_Files/MouseLook.cs
+++ Assets/Player_Files/MouseLook.cs
@@ -10,6 +10,7 @@ public class MouseLook : MonoBehaviour {
 
 		void Update () {
 				if (mn.cammove) {
+						/*
 						camera ();
 						if (hhalf == 1) {
 								transform.RotateAround (head.position, new Vector3 (0, 1, 0), s / 10);
@@ -33,7 +34,10 @@ public class MouseLook : MonoBehaviour {
 						}
 						/*transform.RotateAround (head.position, new Vector3 (1, 0, 0), s / ((-1) * 10) * Input.GetAxis ("Mouse Y"));
 						transform.RotateAround (head.position, new Vector3 (0, 1, 0), s / 10 * Input.GetAxis ("Mouse X"));*/
+			
+						RotateHeadToMousPos ();
 						transform.LookAt (head.position);
+			
 				}
 		}
 	
@@ -42,6 +46,44 @@ public class MouseLook : MonoBehaviour {
 						GetComponent<Rigidbody> ().freezeRotation = true;
 				}
 				mn = GameObject.Find ("Uebergabe").GetComponent<mainmenu> ();
+		}
+	
+		void RotateHeadToMousPos () {
+				float min_left = 0;
+				float max_left = Screen.width / 2 - 50;
+		
+				float min_right = Screen.width / 2 + 50;
+				float max_right = Screen.width;
+		
+				float min_up = Screen.height / 2 + 50;
+				float max_up = Screen.height;
+		
+				float min_down = 0;
+				float max_down = Screen.height / 2 - 50;
+		
+				Vector3 tmp_cam = Input.mousePosition;
+				float tmp_x = tmp_cam.x;
+				float tmp_y = tmp_cam.y;
+		
+				Vector3 angle = Vector3.zero;
+		
+				//Debug.Log (tmp_cam);
+				if (tmp_x > min_right) {
+						angle.y = (90 * ((tmp_x - min_right) / (max_right - min_right)));
+				}
+				if (tmp_x < max_left) {
+						angle.y = -1 * (90 * ((tmp_x - max_left) / (min_left - max_left)));
+				}
+		
+				if (tmp_y > min_up) {
+						angle.x = (90 * ((tmp_y - min_up) / (max_up - min_up)));
+				}
+				if (tmp_y < max_down) {
+						angle.x = -1 * (90 * ((tmp_y - max_down) / (min_down - max_down)));
+				}
+				
+				head.eulerAngles = angle;
+				Debug.Log (angle);
 		}
 	
 		void camera () {
