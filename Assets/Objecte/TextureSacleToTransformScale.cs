a3de548170254903230f5db793d5d4dfa1f41f43
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2015-02-25 13:59:08 +0100

    Wall Scale

--- Assets/Objecte/TextureSacleToTransformScale.cs
+++ Assets/Objecte/TextureSacleToTransformScale.cs
@@ -5,7 +5,7 @@ public class TextureSacleToTransformScale : MonoBehaviour {
 		public MeshRenderer ObjRenderer;
 		// Use this for initialization
 		void Start () {
-				ObjRenderer = GetComponent<MeshRenderer> ();
-				ObjRenderer.material.SetTextureScale ("_MainTex", new Vector2 (transform.localScale.x / 10, transform.localScale.z / 10));
+				ObjRenderer = GetComponentInChildren<MeshRenderer> ();
+				ObjRenderer.material.SetTextureScale ("_MainTex", new Vector2 (transform.localScale.x, transform.localScale.z));
 		}
 }
