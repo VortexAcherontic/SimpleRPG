9d14f903f7fe5061b66359956f999bb9d35a63da
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2015-02-25 13:05:04 +0100

    DoorScript auf 3D

---
+++ Assets/Objecte/Interactable/Door/Door.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public struct DoorOptions {
+		public bool IsOpen;
+		public bool IsLocked;
+		public bool IsTeleporter;
+		public string TeleportToScene;
+		public Vector3 TeleportToPosition;
+}
+
+public class Door : MonoBehaviour {
+		AudioSource sound;
+		Player_Trigger triggerscript;
+		Vector3 startangles;
+		public DoorOptions optionen;
+	
+		void Start () {
+				startangles = transform.localEulerAngles;
+				sound = GetComponent<AudioSource> ();
+				triggerscript = GetComponentInChildren<Player_Trigger> ();
+		}
+	
+		void Update () {
+				Interact ();
+		}
+	
+		void Interact () {
+				if (triggerscript.Player_in_Triger) {
+						if (Input.GetButtonDown ("Interact")) {
+								sound.Play ();
+								optionen.IsOpen = !optionen.IsOpen;
+								if (optionen.IsOpen) {
+										transform.localEulerAngles = new Vector3 (startangles.x, startangles.y + 90, startangles.z);
+								} else {
+										transform.localEulerAngles = new Vector3 (startangles.x, startangles.y, startangles.z);
+								}
+								if (optionen.IsTeleporter) {
+										GameObject.FindWithTag ("Player").transform.position = optionen.TeleportToPosition;
+										Application.LoadLevel (optionen.TeleportToScene);
+								}
+						}
+				}
+		}
+}
---
+++ Assets/Objecte/Player_Trigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Trigger : MonoBehaviour {
+		public bool Player_in_Triger;
+	
+		void OnTriggerEnter (Collider other) {
+				if (other.tag == "Player") {
+						Player_in_Triger = true;
+				}
+		}
+		void OnTriggerExit (Collider other) {
+				if (other.tag == "Player") {
+						Player_in_Triger = false;
+				}
+		}
+}
