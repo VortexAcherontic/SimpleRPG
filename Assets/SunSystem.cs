868ff2e939b706f6a51103ad8b962a49fed6610d
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2015-03-03 12:09:33 +0100

    Monde klappen jetzt auch

--- Assets/SunSystem.cs
+++ Assets/SunSystem.cs
@@ -6,24 +6,27 @@ public class SunSystem : MonoBehaviour {
 	
 		public float GehtAufStunde = 8;
 		public float GehtUnterStunde = 20;
-	
+		
+		float tmp_GehtAufStunde;
+		float tmp_GehtUnterStunde;
 		// Use this for initialization
 		void Start () {
 				TimeSystem = GameObject.Find ("Uebergabe").GetComponent<GameTime> ();
-				GehtAufStunde *= TimeSystem.Stunde;
-				GehtUnterStunde *= TimeSystem.Stunde;
 		}
 	
 		void Update () {
+				tmp_GehtAufStunde = GehtAufStunde * TimeSystem.Stunde;
+				tmp_GehtUnterStunde = GehtUnterStunde * TimeSystem.Stunde;
+			
 				bool IsVisible = false;
-				if (GehtAufStunde < GehtUnterStunde) {
+				if (tmp_GehtAufStunde < tmp_GehtUnterStunde) {
 						//SUN
-						if (TimeSystem.Zeit > GehtAufStunde && TimeSystem.Zeit < GehtUnterStunde) {
+						if (TimeSystem.Zeit > tmp_GehtAufStunde && TimeSystem.Zeit < tmp_GehtUnterStunde) {
 								IsVisible = true;
 						}
 				} else {
 						//Moon
-						if (TimeSystem.Zeit > GehtAufStunde || TimeSystem.Zeit < GehtUnterStunde) {
+						if (TimeSystem.Zeit > tmp_GehtAufStunde || TimeSystem.Zeit < tmp_GehtUnterStunde) {
 								IsVisible = true;
 						}
 				}
@@ -33,13 +36,10 @@ public class SunSystem : MonoBehaviour {
 			
 						// Wenns Aufgeuhrzeit ist dann muss es wohl bei 0 Grad sein
 						// Wenns Untergeuhrzeit ist dann muss es wohl bei 180 Grad sein
-						float DauerSichtbarkeit = ZeitDifferenz (GehtAufStunde, GehtUnterStunde);
-						float StandSichtbarkeit = ZeitDifferenz (GehtAufStunde, TimeSystem.Zeit);
+						float DauerSichtbarkeit = ZeitDifferenz (tmp_GehtAufStunde, tmp_GehtUnterStunde);
+						float StandSichtbarkeit = ZeitDifferenz (tmp_GehtAufStunde, TimeSystem.Zeit);
 						float ProzentualerStand = (100 / DauerSichtbarkeit) * StandSichtbarkeit;
 						float WinkelStand = (ProzentualerStand / 100) * 180;
-						print ("D " + DauerSichtbarkeit);
-						print ("S " + StandSichtbarkeit);			
-						print ("P " + ProzentualerStand);
 						transform.eulerAngles = new Vector3 (WinkelStand, 0, 0);
 						
 				} else {
@@ -53,7 +53,7 @@ public class SunSystem : MonoBehaviour {
 		float ZeitDifferenz (float von, float bis) {
 				float returnValue = 0;
 				if (von > bis) {
-						returnValue += TimeSystem.Tag - von;
+						returnValue += Mathf.Abs (TimeSystem.Tag - von);
 						returnValue += bis;
 				} else {
 						returnValue = bis - von;
